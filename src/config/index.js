62dc089717ae455dcba6e7b82e37bb7ceab2fdd7
李亚军 <dreamoflu@163.com>
2021-05-14 11:16:48 +0800

    test github action

--- src/config/index.js
+++ src/config/index.js
@@ -1,3 +1,11 @@
+/*
+ * @Author: your name
+ * @Date: 2021-05-14 10:47:44
+ * @LastEditTime: 2021-05-14 11:15:10
+ * @LastEditors: your name
+ * @Description: In User Settings Edit
+ * @FilePath: /github-actions/src/config/index.js
+ */
 /**
  * @description 配置项
  * @author 双越
@@ -8,6 +16,7 @@ const { isPrd, isPrdDev } = require('../utils/env')
 // 获取各个环境的不同配置文件
 let fileName = 'dev.js'
 if (isPrdDev) fileName = 'prd-dev.js'
+
 if (isPrd) fileName = 'prd.js'
 
 const conf = require(`./envs/${fileName}`) // eslint-disable-line
